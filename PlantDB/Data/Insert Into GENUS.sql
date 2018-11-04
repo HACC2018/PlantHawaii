59b0719638114f81e3530fbb75a8740250d9456d
Iokepa <ilapera@my.hpu.edu>
2018-11-03 14:19:07 -1000

    dakine

---
+++ PlantDB/Data/Insert Into GENUS.sql
@@ -0,0 +1,14 @@
+------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Marsilea', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Hibiscus', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Coprosma', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Cordia', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Ipomea', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Biancaea', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Tetramolopium', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Peucedanum', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Merosideros', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Argemone', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Thespesia', FUNCTION_ORDER_ID);
+INSERT INTO GENUS(NAME, ORDER_ID) VALUES('Ludwigia', FUNCTION_ORDER_ID);
+------------------------------------------------------------------------------------------------------------------------------------------
---
+++ PlantDB/Data/Insert Into HAWAIIAN_NAME.sql
@@ -0,0 +1,12 @@
+-------------------------------------------------------------------------------------------
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('?ihi?ihi')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Aloalo')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Waimeae')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Aikanene')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Kou')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Koali awa')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Makou')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Lehua')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Pua Kala')
+INSERT INTO HAWAIIAN_NAME(NAME) VALUES('Milo')
+-------------------------------------------------------------------------------------------
