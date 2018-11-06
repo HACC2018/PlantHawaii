8940d070c2bfb055eca533f49a96cbb3c4b5da9b
Iokepa <ilapera@my.hpu.edu>
2018-11-05 19:47:57 -1000

    species fin

--- PlantDB/Data/Insert Into SPECIES.sql
+++ PlantDB/Data/Insert Into SPECIES.sql
@@ -2,7 +2,9 @@
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('villosa', NULL,
 		'This water loving fern can be used in and around ponds and other water features',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Unknown
+
+		Modern Use: Used a drought tolerant groundcover',
 		FUNCTION_GENUS_ID('Marsilea'),
 		FUNCTION_COMMON_NAME_ID('Villous waterclover'),
 		FUNCTION_HAWAIIAN_NAME_ID('?ihi?ihi'),
@@ -13,7 +15,9 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('brackenridgei', 'brackenridgei',
 		'The Yellow Hibiscus can appear as a woody shrub or a small tree. It has sharp prickles and striking yellow flowers.',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Was planted for ornamental use.
+
+		Modern Use: Used as the official flower of Hawai''i',
 		FUNCTION_GENUS_ID('Hibiscus'),
 		FUNCTION_COMMON_NAME_ID('Yellow Hibiscus'),
 		FUNCTION_HAWAIIAN_NAME_ID('Aloalo'),
@@ -24,7 +28,9 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('waimeae', NULL,
 		'Small, gray-barked tree up to 30 feet tall with light green, round/oval leaves and white/pink flower',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Was grown near houses for their flowers.
+
+		Modern Use: Unknown',
 		FUNCTION_GENUS_ID('Hibiscus'),
 		FUNCTION_COMMON_NAME_ID('White Kauai Rosemallow'),
 		FUNCTION_HAWAIIAN_NAME_ID('Waimeae'),
@@ -35,7 +41,9 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('emodeoides', NULL,
 		'This shrub can grow to 6 ft high and 15 ft wide. Naturally found at high elevations, it has cream or yellow flowers, and produces black/dark purple fruit..',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Bark and fruits were used to make leis and yellow and black dyes.
+
+		Modern Use: Still used to make leis and dyes.',
 		FUNCTION_GENUS_ID('Coprosma'),
 		FUNCTION_COMMON_NAME_ID('Black-fruited Coprosma'),
 		FUNCTION_HAWAIIAN_NAME_ID('Aikanene'),
@@ -46,7 +54,9 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('subcordata', NULL,
 		'The Kou is a tree that can grow from 15 to 30 ft high, with a canopy that can spread about 25 ft across. Suitable for roomy landscapes, it has light orange flowers and produces black fruit.',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Was used for making red dyes, leis for young girls, fishing hook containers, bowls, cups, calabashes. Considered one of the best woods for carving, images of gods were made of kou wood. The seeds were used as emergency food, and to treat the thrush disease.
+
+		Modern Use: Still used for carving and making red dyes.',
 		FUNCTION_GENUS_ID('Cordia'),
 		FUNCTION_COMMON_NAME_ID('N/A'),
 		FUNCTION_HAWAIIAN_NAME_ID('Kou'),
@@ -56,10 +66,10 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 	);
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('indica', NULL,
-		'It is poisonous',
+		'A vigorous tender perennial vine native to tropical habitats throughout the world. It bears heart-shaped leaves and rich purple funnel-shaped flowers. It is a poisonous invasive species.',
 		'Early Use: Pounded stems and roots were applied externally to wounds, aches, bruises and broken bones
 
-Modern Use: Still used as a folk medicine',
+		Modern Use: Still used as a folk medicine',
 		FUNCTION_GENUS_ID('Ipomea'),
 		FUNCTION_COMMON_NAME_ID('Oceanblue Morning Glory'),
 		FUNCTION_HAWAIIAN_NAME_ID('Koali awa'),
@@ -69,8 +79,10 @@ Modern Use: Still used as a folk medicine',
 	);
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('decapetala', NULL,
-		'XXXXXXXXXXXXXX',
-		'XXXXXXXXXXXXXX',
+		'A robust, thorny, evergreen shrub  that can grow  2 to 4 m, often forming dense thickets. The leaves are dark green, paler beneath, not glossy, and the flowers are pale yellow, in elongated, erect clusters. Fruit are brown, woody pods, flattened, unsegmented, smooth, sharply beaked at apex',
+		'Early Use: Unknown
+
+		Modern Use: Unknown',
 		FUNCTION_GENUS_ID('Biancaea'),
 		FUNCTION_COMMON_NAME_ID('Cat''s Claw'),
 		FUNCTION_HAWAIIAN_NAME_ID('N/A'),
@@ -80,8 +92,10 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 	);
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('rockii', 'calcisabulorum',
-		'XXXXXXXXXXXXXX',
-		'XXXXXXXXXXXXXX',
+		' A rare species of flowering plant, it appears as a small, mat-forming shrub growing no more than about 10 centimeters tall. Grows only a single flower head.',
+		'Early Use: Unknown
+
+		Modern Use: Unknown',
 		FUNCTION_GENUS_ID('Tetramolopium'),
 		FUNCTION_COMMON_NAME_ID('Dune Tetramolopium'),
 		FUNCTION_HAWAIIAN_NAME_ID('N/A'),
@@ -92,7 +106,9 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('sandwicense', NULL,
 		'A non-woody, herbaceous plant that tends to clump, it can grow to over 3 ft high and 6 ft wide. It produces white flowers.',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Tap root was cooked and eaten by babies and elderly people for medicinal purposes
+
+		Modern Use: Unknown',
 		FUNCTION_GENUS_ID('Peucedanum'),
 		FUNCTION_COMMON_NAME_ID('N/A'),
 		FUNCTION_HAWAIIAN_NAME_ID('Makou'),
@@ -103,7 +119,9 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('polymorpha', NULL,
 		'It ranges in habit from a prostrate shrub to a 100 foot tree. Young bark is smooth and light gray and becomes rough and scaly with age. powder puff like flowers are most often a shade of red, but yellow, orange, salmon, and pink forms are also found.',
-		'XXXXXXXXXXXXXX',
+		'Early Use: Lehua was used to make leis. Medicinal uses for pain relief, thrush treatment, and digestive problems. It was also known as a superior fire wood.
+
+		Modern Use: Leis are still made, while the wood is prized for flooring, furniture, decorative poles, cravings, and ''ukulule keys. Used to make honey and folk remedy teas.',
 		FUNCTION_GENUS_ID('Metrosideros'),
 		FUNCTION_COMMON_NAME_ID('N/A'),
 		FUNCTION_HAWAIIAN_NAME_ID('Lehua'),
@@ -116,7 +134,7 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 		'Hawaiian poppy is non-woody, herbaceous, and tends to clump. It can grow to over 3 ft high, and 2 to 4 ft wide, oer more. Green to gray leaves with yellowish prickles, and white and yellow flowers.',
 		'Early Use: Bright yellow sap and seeds were used to treat toothache and nerve pain.
 
-Modern Use: Used in cow pastures to form beautiful and natural border hedges (many creatures won''t eat pua kala)',
+		Modern Use: Used in cow pastures to form beautiful and natural border hedges (many creatures won''t eat pua kala)',
 		FUNCTION_GENUS_ID('Argemone'),
 		FUNCTION_COMMON_NAME_ID('N/A'),
 		FUNCTION_HAWAIIAN_NAME_ID('Pua Kala'),
@@ -127,7 +145,9 @@ Modern Use: Used in cow pastures to form beautiful and natural border hedges (ma
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('populnea', NULL,
 		'A fast growing, small tree or a large shrub.  It generally reaches a height of 30 feet with a spread of 12 feet.',
-		'Early Use: Was used to make cordage, dye, lei, and house furnishings (food bowels, platters and dishes). Was considered a sacred tree, and young milo leaves and flowers were edible',
+		'Early Use: Was used to make cordage, dye, lei, and house furnishings (food bowels, platters and dishes). Was considered a sacred tree, and young milo leaves and flowers were edible
+
+		Modern Use: Seeds are used to make permanent lei, and the wood is used for to make carvings, bowls and platters.',
 		FUNCTION_GENUS_ID('Thespesia'),
 		FUNCTION_COMMON_NAME_ID('Indian Tulip Tree'),
 		FUNCTION_HAWAIIAN_NAME_ID('Milo'),
@@ -137,8 +157,10 @@ INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_
 	);
 INSERT INTO SPECIES(NAME, SUBSPECIES_NAME, DESCRIPTION, STORY, GENUS_ID, COMMON_NAME_ID, HAWAIIAN_NAME_ID, NATIVE_STATUS_ID, INVASIVE_STATUS_ID, FEDERAL_STATUS_ID)
 	VALUES('octovalvis', NULL,
-		'XXXXXXXXXXXXXX',
-		'XXXXXXXXXXXXXX',
+		'It can grow to be one meter tall on average but is able to grow taller. Its flowers are yellow in appearance, and they produce small capsular fruits containing many minute seeds.',
+		'Early Use: Unknown
+
+		Modern Use: Is known for its anti-ageing properties',
 		FUNCTION_GENUS_ID('Ludwigia'),
 		FUNCTION_COMMON_NAME_ID('Mexian Primrose Willow'),
 		FUNCTION_HAWAIIAN_NAME_ID('N/A'),
