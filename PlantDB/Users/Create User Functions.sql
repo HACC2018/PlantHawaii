82b898af450b0790f81d32ab5f2170b73af9437f
Sean <shadowbomb20@gmail.com>
2018-11-07 18:56:47 -1000

    Fixed User Function - login was deleted and replaced with other...

--- PlantDB/Users/Create User Functions.sql
+++ PlantDB/Users/Create User Functions.sql
@@ -1,11 +1,21 @@
 ------------------------------------------------------------------------------------------------------------------------------------------
-CREATE FUNCTION FUNCTION_USER_CREATE(@USERNAME CHAR(255), @PASSWORD VARCHAR(MAX), @EMAIL CHAR(255))
+CREATE FUNCTION FUNCTION_USER_LOGIN(@USERNAME CHAR(255), @PASSWORD CHAR(8))
 RETURNS INT
 AS
 BEGIN
-    INSERT INTO [USER]([USERNAME], EMAIL, PASSWORD_HASH)
-		VALUES(@USERNAME, @EMAIL, HASHBYTES('SHA2_512', CAST(@PASSWORD AS NVARCHAR(4000))))
-	RETURN 1;
+    DECLARE @userid AS INT
+     SET @userid = (SELECT ID FROM USERNAME WHERE [NAME] = @USERNAME);
+	IF (@userid IS NULL)
+        BEGIN
+            SET @userid = 0;
+            RETURN @userid;
+        END;
+    
+    DECLARE @hash AS BINARY(64);
+    SET @hash = (SELECT PASSWORD_HASH FROM USERNAME WHERE ID = @userid);
+    IF (@hash != @PASSWORD)
+        SET @userid = 0;
+    RETURN @userid;
 END;
 GO
-------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------
